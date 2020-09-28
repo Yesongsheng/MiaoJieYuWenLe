6651480adfffd0d08a68a2158ff1484031eb062f
qwe <1256805764@qq.com>
2020-09-28 15:42:08 +0800

    sad

--- src/main/resources/cn/kgc/Sse.java
+++ src/main/resources/cn/kgc/Sse.java
@@ -1,5 +1,12 @@
 package cn.kgc;
 
 public class Sse {
-
+	private void syso() {
+		// TODO Auto-generated method stub
+		System.out.println("sdadasdads");
+		System.out.println("sdadasdads");
+		System.out.println("sdadasdads");
+		System.out.println("sdadasdads");
+		System.out.println("sdadasdads");
+	}
 }
